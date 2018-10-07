cbf2f5be7118ec3970bb475268e3354b0db88c7d
poovarasanlove <poovarasanlove@gmail.com>
2018-10-07 13:37:00 +0530

    comminting the work file

---
+++ Js01/ass01.js
@@ -0,0 +1,7 @@
+var g= document.getElementById('head');
+function enter(){
+g.innerHTML='Thank you for visiting my page'
+}
+function exit(){
+g.innerHTML='Wellcome to my page'
+}
---
+++ Js02/ass02.js
@@ -0,0 +1,7 @@
+ function ad(){
+var n1= document.getElementById('a').value;
+var n2= document.getElementById('b').value;
+var n3= Number(n1)+Number(n2);
+document.getElementById('c').innerHTML=n3;
+}
+
